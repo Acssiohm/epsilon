59e5757d4e7214365aafe8f1174e657d48216e2d
Farsi Assim <hamriassim@gmail.com>
2021-08-01 07:57:16 +0200

    my first app test

---
+++ apps/my_app/app.cpp
@@ -0,0 +1,36 @@
+#include "app.h"
+#include <apps/i18n.h>
+
+extern "C" {
+#include <assert.h>
+}
+
+namespace My {
+
+I18n::Message App::Descriptor::name() {
+  return I18n::Message::MyApp;
+}
+
+I18n::Message App::Descriptor::upperName() {
+  return I18n::Message::MyCapApp;
+}
+
+const Image * App::Descriptor::icon() {
+  return ImageStore::MyApp;
+}
+
+App * App::Snapshot::unpack(Container * container) {
+  return new (container->currentAppBuffer()) App(this);
+}
+
+App::Descriptor * App::Snapshot::descriptor() {
+  static Descriptor descriptor;
+  return &descriptor;
+}
+
+App::App(Snapshot * snapshot) :
+  ::App(snapshot, nullptr, I18n::Message::Warning),
+{
+}
+
+}
---
+++ apps/my_app/app.h
@@ -0,0 +1,29 @@
+#ifndef MY_APP_H
+#define MY_APP_H
+
+#include "./shared_app.h"
+
+
+namespace My {
+
+class App : public ::App {
+public:
+  class Descriptor : public ::App::Descriptor {
+  public:
+    I18n::Message name() override;
+    I18n::Message upperName() override;
+    const Image * icon() override;
+  };
+  class Snapshot : public ::SharedApp::Snapshot {
+  public:
+    App * unpack(Container * container) override;
+    Descriptor * descriptor() override;
+  };
+  TELEMETRY_ID("My");
+private:
+  App(Snapshot * snapshot);
+};
+
+}
+
+#endif
