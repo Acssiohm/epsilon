e56e6c3a9b762aeba741f1ac97b469fd3493e16b
Farsi Assim <hamriassim@gmail.com>
2021-08-02 01:14:08 +0200

    add a cell

--- apps/my_app/content.cpp
+++ apps/my_app/content.cpp
@@ -19,7 +19,7 @@ namespace My {
   }
   bool MyController::handleEvent(Ion::Events::Event event){
     int rowIndex = selectedRow();
-    if ((event == Ion::Events::Left || event == Ion::Events::Right || event == Ion::Events::Minus || event == Ion::Events::Plus)) {
+    if (rowIndex < k_numberOfGaugeCells && (event == Ion::Events::Left || event == Ion::Events::Right || event == Ion::Events::Minus || event == Ion::Events::Plus)) {
       float delta = 0.1;
       float direction = (event == Ion::Events::Right || event == Ion::Events::Plus) ? delta : -delta;
       float lvl = ( (GaugeView *) m_gaugeCells[rowIndex].accessoryView() ) -> level();
@@ -27,14 +27,23 @@ namespace My {
       m_table.reloadCellAtLocation(m_table.selectedColumn(), m_table.selectedRow());
       return true;
     }
+    if (rowIndex == k_numberOfGaugeCells && (event == Ion::Events::OK || event == Ion::Events::EXE)){
+      bool state = ( (switchView *) m_switch_cell.accessoryView()) -> state()
+      ( (switchView *) m_switch_cell.accessoryView()) -> setState( !state );
+    }
     return false;
   }
   void MyController::willDisplayCellForIndex(HighlightCell * cell, int index) {
+    if (index == k_numberOfGaugeCells){
+      MessageTableCellWithSwitch * mySwitchCell = (MessageTableCellWithSwitch *)cell;
+      mySwitchCell -> setMessage(messageAtIndex(index));  
+      return ;
+    }
     MessageTableCellWithGauge * myGaugeCell = (MessageTableCellWithGauge *)cell;
     myGaugeCell->setMessage(messageAtIndex(index));
   }
   int MyController::numberOfRows() const {
-    return k_numberOfGaugeCells;
+    return k_numberOfGaugeCells + 1;
   }
   KDCoordinate MyController::rowHeight(int j) {
     return Metric::ParameterCellHeight;
@@ -49,10 +58,16 @@ namespace My {
     return &m_gaugeCells[index];
   }
   int MyController::reusableCellCount(int type) {
-    return k_numberOfGaugeCells;
+    if (type == 0 ){
+      return k_numberOfGaugeCells;
+    }
+    return 1;
   }
   int MyController::typeAtLocation(int i, int j) {
-    return 0;
+    if (j < k_numberOfGaugeCells){
+      return 0;
+    }
+    return 1;
   }
   void MyController::didBecomeFirstResponder() {
     if (selectedRow() < 0) {
