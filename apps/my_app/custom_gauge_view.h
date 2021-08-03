4bbfab85b0697bfc7c67a671acd4a802f9dbc5a6
Farsi Assim <hamriassim@gmail.com>
2021-08-03 17:13:42 +0200

    fix bug

--- apps/my_app/custom_gauge_view.h
+++ apps/my_app/custom_gauge_view.h
@@ -4,10 +4,11 @@
 #include "../../escher/src/gauge_view.cpp"
 
 class CustomGaugeView : public GaugeView {
+public:
 	CustomGaugeView();
 	void drawRect(KDContext * ctx, KDRect rect) const override;
 	void setColor(KDColor color);
-
+protected:
 	KDColor m_color;
 };
 
